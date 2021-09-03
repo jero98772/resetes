3e8f7e4b47b962879ac1741943643112dbb4d319
jero98772 <jero98772@protonmail.com>
2021-09-03 12:42:32 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-"
+"""
+gas - 2021 - por jero98772
+gas - 2021 - by jero98772
+"""
+from setuptools import setup, find_packages
+setup(
+	name='gas',
+	version='1.0.0 beta',
+	license='GPLv3',
+	author_email='jero98772@protonmail.com',
+	author='jero98772',
+	description='resetes',
+	url='',
+	packages=find_packages(),
+    install_requires=['Flask'],
+    include_package_data=True,
+	)
---
+++ tools/tools.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-"
+"""
+resetes - 2021 - por jero98772
+resetes - 2021 - by jero98772
+"""
+from flask import request
+from hashlib import  sha256
+import base64
+def multrequest(items):	
+	values = []
+	for item in items:		
+		item = request.form.get(item)
+		try:
+			item = float(item)
+		except:	
+			item = str(item)
+		values.append(item)
+	return values
+def enPassowrdStrHex(password):
+	password = str(password)
+	hashPassowrd = str(sha256(password.encode('utf-8')).hexdigest())
+	return hashPassowrd
